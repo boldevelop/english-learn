b38595a5c28f656e42b719b0c99a4aba12c2950d
Владислав Болдаков <boldakov.vladislav@gmail.com>
2020-05-20 17:40:18 +0700

    view(translate): stylization translate page

--- src/js/components/Translate.js
+++ src/js/components/Translate.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Group, CardGrid, Card, Text, Title, Div, Separator } from "@vkontakte/vkui";
+import { Group, CardGrid, Title, Banner } from "@vkontakte/vkui";
 import {shallowEqual, useDispatch, useSelector} from "react-redux";
 import {setSelectedStroke, toggleModalCard} from "../actions";
 
 export const Translate = () => {
+    const translate = useSelector(state => state.translate, shallowEqual)
     const dispatch = useDispatch()
 
-    const translate = useSelector(state => state.translate, shallowEqual)
-
-    const onClickStroke = async (strokeData) => {
+    const onClickStroke = strokeData => {
         dispatch(setSelectedStroke(strokeData))
         dispatch(toggleModalCard('history-settings'))
     }
@@ -18,17 +17,18 @@ export const Translate = () => {
             <CardGrid>
                 {!!translate.text.length && translate.text.map( item =>
                     item.type ? (
-                        <Title key={Math.random()} level="2" weight="semibold">
+                        <Title key={Math.random()} level="2" weight="regular" style={{ marginTop: 20, color: 'var(--my-accent)' }}>
                             {item.type}
                         </Title>
                     ) : (
-                        <Card key={Math.random()} size="l" mode="shadow" onClick={() => onClickStroke(item)}>
-                            <Div>
-                                <Title level="3" weight="semibold">{item.en}</Title>
-                                <Separator style={{ margin: '12px 0' }} />
-                                <Text weight="regular">{item.ru}</Text>
-                            </Div>
-                        </Card>
+                        <Banner
+                            key={Math.random()}
+                            className={`custom-banner custom-banner_translate`}
+                            header={item.en}
+                            subheader={item.ru}
+                            asideMode="expand"
+                            onClick={() => onClickStroke(item)}
+                        />
                     )
                 )}
 
